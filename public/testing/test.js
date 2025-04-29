c45979b610d1125772838176612f6f65dc2bf2bc
Zongcha Lo <zlo2@wisc.edu>
2025-04-29 15:18:40 -0500

    added delay in testings

--- public/testing/test.js
+++ public/testing/test.js
@@ -20,12 +20,12 @@ async function testLogin() {
   await page.goto("https://alphaphiomega-5bee2.web.app/");
 
   // Open the navigation menu
- 
+
   await page.click(".navbar-burger");
 
   // Click the Sign In button
 
-  await page.click('button[onclick="openModal(\'signin\')"]');
+  await page.click("button[onclick=\"openModal('signin')\"]");
 
   // Fill out the login form
   await page.type("#emailInput", "test2@test.com");
@@ -34,20 +34,25 @@ async function testLogin() {
   // Submit the login form
   await page.click("#authButton");
 
+  // Force Delay
+  await new Promise((r) => setTimeout(r, 1000));
+
   // Navigate to the Contact Us page
   await page.click("#contact");
-  
 
   // Fill out the feedback form
   await page.type("#f_email", "test2@test.com");
-  await page.type("#feedback", "This is an automated test feedback submission.");
+  await page.type(
+    "#feedback",
+    "This is an automated test feedback submission."
+  );
 
   // Submit the feedback form
   await page.click("#feedback_form button[type='submit']");
 
-  console.log("✅ Feedback Submission Test Passed!");
+  console.log("Feedback Submission Test Passed!");
 
-  await browser.close();
+  // await browser.close();
 }
 
 testLogin();
